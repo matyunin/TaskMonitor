a8bff8f41165a9e460cbe69fb85eead061394e11
matyunin <free.all.bums@gmail.com>
2013-05-12 01:04:42 +0400

    -

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,9 +33,10 @@ Taskmonitor::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = false
+  config.assets.debug = true
 
   config.assets.compile = false
+  config.serve_static_assets = false
 
   # Devise settings
   config.action_mailer.default_url_options = { :host => 'localhost' }
