6d608a33e617cca0bd2446c9e1caee5b80e413b5
matyunin <free.all.bums@gmail.com>
2013-05-12 01:01:02 +0400

    -

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,7 +33,7 @@ Taskmonitor::Application.configure do
   config.assets.compress = false
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 
   config.assets.compile = false
 
