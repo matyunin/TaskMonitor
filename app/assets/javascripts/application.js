46a04c2449b7ee9ef07f671e1a9c849c7f813bd3
matyunin <free.all.bums@gmail.com>
2013-05-12 00:21:50 +0400

    -

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require jquery_ujs
 //= require jquery
 //= require bootstrap.min
-//= require jquery_ujs
