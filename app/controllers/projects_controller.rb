cab01d631f5a65604a1f5caefe4b6f16e8531e31
matyunin <free.all.bums@gmail.com>
2013-05-11 23:14:09 +0400

    projects list template fix

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,6 +2,7 @@ class ProjectsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    @projects = current_user.projects
+    @user = current_user
+    @projects = @user.projects
   end
 end
