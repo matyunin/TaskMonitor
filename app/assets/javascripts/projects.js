67aa75776872f7e4540becbe88d7aafa48c2164b
matyunin <free.all.bums@gmail.com>
2013-05-12 05:43:49 +0400

    -

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,3 +1,20 @@
 $(function(){
-    alert(1);
+    var BTN_TASK_ADD = $('#add_task');
+
+    var FN_TASK_ADD = function(e){
+        e.preventDefault();
+        $this = $(this);
+        $.fancybox({
+            href: $this.attr('href'),
+            helpers : {
+                overlay : {
+                    css : {
+                        'background' : 'rgba(58, 42, 45, 0.95)'
+                    }
+                }
+            }
+        });
+    }
+
+    BTN_TASK_ADD.click(FN_TASK_ADD);
 })
