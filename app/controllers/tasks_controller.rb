c54ca56c5f73e31b4d8daaef18744cacb512048e
matyunin <free.all.bums@gmail.com>
2013-05-12 19:21:04 +0400

    -

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,10 +9,15 @@ class TasksController < ApplicationController
   def create
     render :layout => false
 
+    start = params[:start].to_date.to_time.to_i.to_s
+    duration = (params[:duration].to_i * 24 * 3600).to_s
+    finish = start.to_i + (params[:priority].to_i * duration.to_i) / 100
+
     @task = Task.new(
         :name         => params[:name],
-        :start        => params[:start].to_date.to_time.to_i.to_s,
-        :duration     => (params[:duration].to_i * 24 * 3600).to_s,
+        :start        => start,
+        :finish       => finish,
+        :duration     => duration,
         :priority     => params[:priority],
         :project      => params[:project].to_i,
         :description  => params[:description],
@@ -27,14 +32,47 @@ class TasksController < ApplicationController
   end
 
   def recalculate(project_id)
+    durations = {}
+    task_ids = []
+    timestamp = 0
+
     tasks = Task.where(:project => project_id)
-    common_points = tasks.map {|task| [task.start.to_i]}
+    common_points = tasks.map {|task| [task.start.to_i]}.uniq!.sort!
+    task_ids = tasks.map {|task| [task._id]}
+    timestamp = common_points.first
+
+    while task_ids.count > 0 do
+      plot = Plot.new(
+          :time => timestamp
+      )
+
+      tasks_intersects = self::task_intersects(tasks, task_ids, timestamp)
+
+      tasks.each do |task|
+        priority = task.priority
+
+
+
+        plot.priority = self::is_working_day?(timestamp) ? priority : 0
+      end
+
+      timestamp += 3600 * 24
+    end
+
 
     puts common_points.inspect
   end
 
+  def task_intersects(all_tasks, available_ids, timestamp)
+    all_tasks.select {|task| task if task.start <= timestamp and available_ids.include? task._id}
+  end
+
+  def is_working_day?(timestamp)
+    self::working_days(timestamp).include? timestamp
+  end
+
   def working_days(timestamp)
     require 'date'
-    ( (Date.strptime(timestamp,'%s').beginning_of_month)..(Date.strptime(timestamp,'%s').end_of_month) ).select {|d| (1..5).include?(d.wday) }.map {|t| t.to_time.to_i}
+    ( (Date.strptime(timestamp.to_s,'%s').beginning_of_month)..(Date.strptime(timestamp.to_s,'%s').end_of_month) ).select {|d| (1..5).include?(d.wday) }.map {|t| t.to_time.to_i}
   end
 end
