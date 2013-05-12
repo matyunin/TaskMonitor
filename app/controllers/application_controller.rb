5f4217679b294229e5e34e565f9f56bf09e22076
matyunin <free.all.bums@gmail.com>
2013-05-12 05:21:35 +0400

    -

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :your_function
+
+  def your_function
+    @controller = controller_name
+    @action = action_name
+  end
 end
