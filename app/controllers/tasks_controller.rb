99cbbec0fc31a5f90a82f05bab0e3f645468e2db
matyunin <free.all.bums@gmail.com>
2013-05-12 16:04:29 +0400

    -

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,6 +9,7 @@ class TasksController < ApplicationController
     @task = Task.new(
         :name         => params[:name],
         :start        => params[:start].to_date.to_time.to_i.to_s,
+        :finish       => '',
         :duration     => (params[:duration].to_i * 24 * 3600).to_s,
         :priority     => params[:priority],
         :project      => params[:project].to_i,
@@ -20,6 +21,7 @@ class TasksController < ApplicationController
   end
 
   def recalc
-    puts 'aaa!!!'
+    tasks = Task.where(:project => 1)
+    common_points = tasks.map {|task| [task.start.to_i, task.points.time.to_i]}
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,12 @@
 class Task
   include MongoMapper::Document
 
-  key :name, String
-  key :start,  String
-  key :duration,  String
-  key :priority,  Integer
-  key :project,  Integer
-  key :description,  String
-  key :points,  Array
+  key :name,        String
+  key :start,       String
+  key :finish,      String
+  key :duration,    String
+  key :priority,    Integer
+  key :project,     Integer
+  key :description, String
+  key :points,      Array
 end
