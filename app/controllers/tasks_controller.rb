868569c294927f8b2ed3c5724ca11e7d2e0939de
matyunin <free.all.bums@gmail.com>
2013-05-12 09:12:06 +0400

    -

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,14 @@ class TasksController < ApplicationController
 
   def create
     render :layout => false
-    task = Task.new(:name => 'Brandon')
+    task = Task.new(
+        :name         => params[:name],
+        :start        => params[:start].to_date.to_time.to_i,
+        :duration     => params[:duration] * 24 * 3600,
+        :priority     => params[:priority],
+        :description  => params[:description]
+    )
     task.save!
+    respond_to :json
   end
 end
