5bcc6c1044a2999f92f34d3ece3ce582d6444dcb
matyunin <free.all.bums@gmail.com>
2013-05-12 07:55:58 +0400

    -

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,21 +11,3 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery_ujs
-//= require_self
-
-$.fn.serializeObject = function()
-{
-    var o = {};
-    var a = this.serializeArray();
-    $.each(a, function() {
-        if (o[this.name] !== undefined) {
-            if (!o[this.name].push) {
-                o[this.name] = [o[this.name]];
-            }
-            o[this.name].push(this.value || '');
-        } else {
-            o[this.name] = this.value || '';
-        }
-    });
-    return o;
-};
--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -3,6 +3,23 @@ $(function(){
     var FORM_TASK_NEW = $('.task-new');
     var INP_DATAPICKER = $('.datapick');
 
+    $.fn.serializeObject = function()
+    {
+        var o = {};
+        var a = this.serializeArray();
+        $.each(a, function() {
+            if (o[this.name] !== undefined) {
+                if (!o[this.name].push) {
+                    o[this.name] = [o[this.name]];
+                }
+                o[this.name].push(this.value || '');
+            } else {
+                o[this.name] = this.value || '';
+            }
+        });
+        return o;
+    };
+
     var FN_TASK_ADD = function(e){
         e.preventDefault();
         $this = $(this);
