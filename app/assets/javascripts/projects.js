968d48521994641ae14625382f79cc868d8758e0
matyunin <free.all.bums@gmail.com>
2013-05-12 15:22:06 +0400

    -

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -35,7 +35,7 @@ $(function(){
             helpers : {
                 overlay : {
                     css : {
-                        'background' : 'rgba(255, 255, 255, 0.1)'
+                        'background' : 'rgba(255, 255, 255, 0.3)'
                     }
                 }
             }
